79bc486fcda6873a8024bcee70821bd7b6bfe406
vbetts <toribetts@gmail.com>
2015-01-20 21:42:03 -0500

    Responding to tweets using blankfiller class

--- FillInTheBot.py
+++ FillInTheBot.py
@@ -4,11 +4,13 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy import API
-import random
 import configparser
 import json
+import blankfiller
 
-config = ConfigParser.ConfigParser()
+bfill = blankfiller.BlankFiller()
+
+config = configparser.ConfigParser()
 config.read('.twitter')
 
 consumer_key = config.get('apikey', 'key')
@@ -19,70 +21,47 @@ stream_rule = config.get('app', 'rule')
 account_screen_name = config.get('app', 'account_screen_name').lower() 
 account_user_id = config.get('app', 'account_user_id')
 
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+twitterApi = API(auth)
 
-nouns = []
-verbs = []
-actions = []
-adjectives = []
-animals = []
-colours = []
-sports = []
+class ReplyToTweet(StreamListener):
 
-nFILENAME = "nounlist.txt"
-vFILENAME = "verblist.txt"
-acFILENAME = "actionlist.txt"
-adFILENAME = "adjectivelist.txt"
-anFILENAME = "animallist.txt"
-cFILENAME = "colorlist.txt"
-sFILENAME = "sportlist.txt"
-
-#Read from the text files and add each word to our defined lists
-def Create_List(filename, list):
-    with open(filename) as openfile:
-        for word in openfile:
-            #Python appends a newline character when reading from a text file. We have to strip that character.
-            wordFix = word.replace('\n', '')
-            list.append(wordFix)
-        return list
-
-Create_List(nFILENAME, nouns)
-Create_List(vFILENAME, verbs)
-Create_List(acFILENAME, actions)
-Create_List(adFILENAME, adjectives)
-Create_List(anFILENAME, animals)
-Create_List(cFILENAME, colours)
-Create_List(sFILENAME, sports)
-
-print ("[v] = verb      [n] = noun      [ac] = action    [ad] = adjective")
-print ("[an] = animal       [c] = colour        [s] = sport")
-print ("")
-
-userSentence = input("Enter a sentence! Use the above key to create 'blanks': ")
-
-def fill_in_the_blanks(sentence):
-    
-    while True:
-        #For each loop, select a new random number for each blank
-        nounNum = random.randint(0, len(nouns))
-        verbNum = random.randint(0, len(verbs))
-        actionNum = random.randint(0, len(actions))
-        adjNum = random.randint(0, len(adjectives))
-        anNum = random.randint(0, len(animals))
-        colNum = random.randint(0, len(colours))
-        spNum = random.randint(0, len(sports))
+    def on_data(self, data):
+        print (data)
+        tweet = json.loads(data.strip())
         
-        #Find each incidence of the word key and replace it with a randomly generated word from each list
-        text = sentence.replace('[n]' and '[N]', nouns[nounNum], 1)\
-            .replace('[ad]' and '[AD]' and '[Ad]' and '[aD]', adjectives[adjNum], 1)\
-            .replace('[v]' and '[V]', verbs[verbNum], 1)\
-            .replace('[ac]' and '[AC]' and '[Ac]' and '[aC]', actions[actionNum], 1)\
-            .replace('[an]' and '[AN]' and '[An]' and '[aN]', animals[anNum], 1)\
-            .replace('[c]' and '[C]', colours[colNum], 1)\
-            .replace('[s]' and '[S]', sports[spNum], 1)
-        if text == sentence:
-            break
-        sentence = text
-    return text
+        retweeted = tweet.get('retweeted')
+        from_self = tweet.get('user',{}).get('id_str','') == account_user_id
 
-print(fill_in_the_blanks(userSentence))
+        if retweeted is not None and not retweeted and not from_self:
+
+            tweetId = tweet.get('id_str')
+            screenName = tweet.get('user',{}).get('screen_name')
+            tweetText = tweet.get('text')
+
+            replyText = '.@' + screenName + ' ' + bfill.fill_in_the_blanks(tweetText)
+
+            #check if repsonse is over 140 char
+            if len(replyText) > 140:
+                replyText = replyText[0:137] + '...'
+
+            print('Tweet ID: ' + tweetId)
+            print('From: ' + screenName)
+            print('Tweet Text: ' + tweetText)
+            print('Reply Text: ' + replyText)
+
+            # If rate limited, the status posts should be queued up and sent on an interval
+            twitterApi.update_status(replyText, tweetId)
+
+    def on_error(self, status):
+        print (status)
+
+
+if __name__ == '__main__':
+    streamListener = ReplyToTweet()
+    twitterStream = Stream(auth, streamListener)
+    twitterStream.userstream(_with='user')
+
+
 
---
+++ blankfiller.py
@@ -0,0 +1,69 @@
+import random
+
+class BlankFiller:
+    nouns = []
+    verbs = []
+    actions = []
+    adjectives = []
+    animals = []
+    colours = []
+    sports = []
+    
+    nFILENAME = "nounlist.txt"
+    vFILENAME = "verblist.txt"
+    acFILENAME = "actionlist.txt"
+    adFILENAME = "adjectivelist.txt"
+    anFILENAME = "animallist.txt"
+    cFILENAME = "colorlist.txt"
+    sFILENAME = "sportlist.txt"
+    
+    #Read from the text files and add each word to our defined lists
+    def Create_List(self, filename, list):
+        with open(filename) as openfile:
+            for word in openfile:
+                #Python appends a newline character when reading from a text file. We have to strip that character.
+                wordFix = word.replace('\n', '')
+                list.append(wordFix)
+            return list
+    def __init__(self):
+        self.Create_List(self.nFILENAME, self.nouns)
+        self.Create_List(self.vFILENAME, self.verbs)
+        self.Create_List(self.acFILENAME, self.actions)
+        self.Create_List(self.adFILENAME, self.adjectives)
+        self.Create_List(self.anFILENAME, self.animals)
+        self.Create_List(self.cFILENAME, self.colours)
+        self.Create_List(self.sFILENAME, self.sports)
+    
+    #print ("[v] = verb      [n] = noun      [ac] = action    [ad] = adjective")
+    #print ("[an] = animal       [c] = colour        [s] = sport")
+    #print ("")
+    
+    #userSentence = input("Enter a sentence! Use the above key to create 'blanks': ")
+    
+    def fill_in_the_blanks(self, sentence):
+        
+        while True:
+            #For each loop, select a new random number for each blank
+            nounNum = random.randint(0, len(self.nouns))
+            verbNum = random.randint(0, len(self.verbs))
+            actionNum = random.randint(0, len(self.actions))
+            adjNum = random.randint(0, len(self.adjectives))
+            anNum = random.randint(0, len(self.animals))
+            colNum = random.randint(0, len(self.colours))
+            spNum = random.randint(0, len(self.sports))
+            
+            #Find each incidence of the word key and replace it with a randomly generated word from each list
+            text = sentence.replace('[n]', self.nouns[nounNum], 1)\
+                .replace('[ad]', self.adjectives[adjNum], 1)\
+                .replace('[v]', self.verbs[verbNum], 1)\
+                .replace('[ac]', self.actions[actionNum], 1)\
+                .replace('[an]', self.animals[anNum], 1)\
+                .replace('[c]', self.colours[colNum], 1)\
+                .replace('[s]', self.sports[spNum], 1)
+            if text == sentence:
+                break
+            sentence = text
+        return text
+    
+    #print(fill_in_the_blanks(userSentence))
+    
